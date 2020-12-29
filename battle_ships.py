41e603102306d370fa52de3dd207fee8db178f93
Igal Bibliv <igalbibliv@gmail.com>
2020-12-29 14:22:37 +0200

    Added intiail battle_ships file

---
+++ battle_ships.py
@@ -0,0 +1,13 @@
+"""
+Description:    File represents the whole battleships game
+Creator:    Igal Bibliv
+Last Modification:  29/12/2020 - Created
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
