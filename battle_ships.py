7d6a37885c168adea1941fa77797c70ee9f0c7ea
Igal Bibliv <igalbibliv@gmail.com>
2020-12-30 11:54:09 +0200

    Added attack and get_attacked in BattleShips class

--- battle_ships.py
+++ battle_ships.py
@@ -2,8 +2,35 @@
 Name:   battle_ships.py
 Description:    File represents the whole battleships game
 Creator:    Igal Bibliv
-Last Modification:  29/12/2020 - Created
+Last Modification:  30/12/2020 - edited
 """
+import communicator
+import player_interface
+import table_handler
+
+
+class BattleShips:
+    def __init__(self, game_communicator, interface, tables):
+        self.game_communicator = game_communicator
+        self.interface = interface
+        self.tables = tables
+
+    def get_attacked(self):
+        """
+        Function handles an enemy player's attack
+        """
+        enemy_request = self.game_communicator.recv_msg()
+        attack_result = self.tables.handle_attack(enemy_request)
+        self.game_communicator.send_msg(attack_result)
+
+    def attack(self):
+        """
+        Function handles the current player's attack
+        """
+        attack_request = self.interface.get_players_wanted_attack()
+        self.game_communicator.send_msg(attack_request)
+        result = self.game_communicator.recv_msg()
+        self.tables.handle_result(attack_request.attacked_tile, result)
 
 
 def main():
