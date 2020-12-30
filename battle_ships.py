cacdb50efc1f1553e1bf38a9d1ceb27096ba5d83
Igal Bibliv <igalbibliv@gmail.com>
2020-12-30 14:12:03 +0200

    started play function in battle_ships

--- battle_ships.py
+++ battle_ships.py
@@ -10,10 +10,11 @@ import table_handler
 
 
 class BattleShips:
-    def __init__(self, game_communicator, interface, tables):
+    def __init__(self, game_communicator, interface, tables, is_playing):
         self.game_communicator = game_communicator
         self.interface = interface
         self.tables = tables
+        self.is_playing = is_playing
 
     def get_attacked(self):
         """
@@ -31,3 +32,11 @@ class BattleShips:
         self.game_communicator.send_msg(attack_request)
         result = self.game_communicator.recv_msg()
         self.tables.handle_result(attack_request.attacked_tile, result)
+
+    def play(self):
+        while True:
+            if self.is_playing:
+                self.attack()
+            else:
+                self.get_attacked()
+            self.is_playing = not self.is_playing
